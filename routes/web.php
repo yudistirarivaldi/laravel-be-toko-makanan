06dc7a5c21a09f9e72cf2d9bec82faa8d18d461d
yudistirarivaldi <yudistirarivaldii1234@gmail.com>
2023-01-13 21:39:06 +0800

    Add dashboard view and controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\MidtransController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,19 +15,16 @@ use App\Http\Controllers\API\MidtransController;
 |
 */
 
+// homepage
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+// dashboard
+Route::prefix('dashboard')->middleware(['auth:sanctum', 'admin'])->group(function() {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
+
 
 // Midtrang page
 Route::get('midtrans/success', [MidtransController::class, 'success']);
