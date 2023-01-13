dea88f7ee1542bc3c80fd55560e8b09ed2809729
yudistirarivaldi <yudistirarivaldii1234@gmail.com>
2023-01-13 15:42:34 +0800

    Fix bug food controller

--- app/Http/Controllers/API/FoodController.php
+++ app/Http/Controllers/API/FoodController.php
@@ -26,57 +26,50 @@ class FoodController extends Controller
         {
             $food = Food::find($id);
 
-            // jika food ada
             if($food)
             {
                 return ResponseFormatter::success(
                     $food,
-                    'Data produk berhasil di ambil'
-                );
-            } else {
-                return ResponseFormatter::error(
-                    null,
-                    'Data produk tidak ada',
-                    404
+                    'Data produk berhasil diambil'
                 );
             }
-
-            $food = Food::query();
-
-            if($name)
-            {
-                $food->where('name','like','%' . $name . '%');
-            }
-
-            if($types)
-            {
-                $food->where('types','like','%' . $types . '%');
-            }
-
-            if($price_from)
-            {
-                $food->where('price_from','like','%' . $price_from . '%');
-            }
-
-            if($price_to)
-            {
-                $food->where('price_to','like','%' . $price_to . '%');
-            }
-
-            if($rate_from)
-            {
-                $food->where('rate_from','like','%' . $rate_from . '%');
-            }
-
-            if($rate_to)
-            {
-                $food->where('rate_to','like','%' . $rate_to . '%');
-            }
-
-            return ResponseFormatter::success(
-                $food->paginate($limit),
-                'Data list produk berhasil diambil'
-            );
         }
+
+        $food = Food::query();
+
+        if($name)
+        {
+            $food->where('name', 'like', '%' . $name . '%');
+        }
+
+        if($types)
+        {
+            $food->where('types', 'like', '%' . $types . '%');
+        }
+
+        if($price_from)
+        {
+            $food->where('price', '>=', $price_from);
+        }
+
+        if($price_to)
+        {
+            $food->where('price', '<=', $price_to);
+        }
+
+        if($rate_from)
+        {
+            $food->where('rate', '>=', $rate_from);
+        }
+
+        if($rate_to)
+        {
+            $food->where('rate', '<=', $rate_to);
+        }
+
+        return ResponseFormatter::success(
+            $food->paginate($limit),
+            'Data list produk berhasil diambil'
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\API\MidtransController;
-use App\Http\Controllers\API\TransactionController;
-use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\FoodController;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\MidtransController;
+use App\Http\Controllers\API\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
